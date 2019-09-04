88968912848d2611df840e98b50a4f22fc5fdab2
Sasukedevlopper <52262018+sasukedevlopper@users.noreply.github.com>
2019-09-04 01:24:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,27 +12,5 @@ client.on('message', msg => {
 });
 
 
-});
-const moment = require('moment')
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on('ready', function(){
-    var ms = 100000 ;
-    var setGame = [`I'am In Dev`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/SasukeGaming03`);
-    }, ms);100000
-
-});
 
 client.login(process.env.BOT_TOKEN);
